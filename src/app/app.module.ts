3832cef1b4e0696a19471c016b96778597a95f33
Asmaa Elabid <asmaa9920@hotmail.com>
2024-03-13 10:15:18 +0000

    Add Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HeroesComponent, HeroDetailComponent],
-  providers: [],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    MessagesComponent
+  ],
+  imports: [
+    HeroDetailComponent,
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [
+    // no need to place any providers due to the `providedIn` flag...
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule { }
