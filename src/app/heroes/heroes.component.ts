8209fbd9c8921ce98eaf13b354e62305fda8c169
Asmaa Elabid <asmaa9920@hotmail.com>
2024-03-13 10:01:42 +0000

    Create a feature component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,14 +3,17 @@ import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
   standalone: true,
-  imports: [NgFor, FormsModule, NgIf, UpperCasePipe],
+  imports: [NgFor, FormsModule, NgIf, UpperCasePipe,  HeroDetailComponent],
+  
 })
+
 export class HeroesComponent {
   hero: Hero = {
     id: 1,
@@ -18,7 +21,7 @@ export class HeroesComponent {
   };
   heroes = HEROES;
   selectedHero?: Hero;
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
